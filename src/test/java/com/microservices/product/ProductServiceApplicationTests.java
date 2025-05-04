a06e910f1810a9c92d3ad8ed54ffde48bb1d859c
AleksandrBut <alex638000@gmail.com>
2025-05-04 15:15:30 +0300

    test(product): create integration test using testcontainers

--- src/test/java/com/microservices/product/ProductServiceApplicationTests.java
+++ src/test/java/com/microservices/product/ProductServiceApplicationTests.java
@@ -1,15 +1,50 @@
 package com.microservices.product;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 class ProductServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @LocalServerPort
+    private int port;
 
+    @BeforeEach
+    void setUp() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
+    @Test
+    void shouldCreateProduct() {
+        String requestBody = """
+                {
+                    "name": "Iphone 1",
+                    "description": "Iphone 1 description",
+                    "price" : 123.456
+                }
+                """;
+
+        RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .when()
+                .post("api/product")
+                .then()
+                .statusCode(201)
+                .body("id", notNullValue())
+                .body("name", equalTo("Iphone 1"))
+                .body("description", equalTo("Iphone 1 description"))
+                .body("price", equalTo(123.456F));
+    }
 }
